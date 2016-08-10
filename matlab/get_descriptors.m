71178e8c3cc1bbae0c077d343254233df66c8eb9
Karel Lenc <lenckarel@gmail.com>
2016-08-10 19:23:41 +0100

    get_descriptors now computes and caches descriptors for the whole image.

--- matlab/get_descriptors.m
+++ matlab/get_descriptors.m
@@ -1,34 +1,27 @@
-function [descriptors, proctime] = get_descriptors( imdb, signature, descfun, varargin )
+function descriptors = get_descriptors( imdb, signature, descfun, varargin )
 % GET_DESCRIPTORS Compute descriptors and eventually cache them
 opts.cachePath = fullfile(fileparts(mfilename('fullpath')), 'data', 'descriptors');
 opts.cacheName = '';
 opts = vl_argparse(opts, varargin);
 
-if ~isempty(opts.cacheName)
-  signParts = strsplit(signature, '.');
-  cachePath = fullfile(opts.cachePath, opts.cacheName, ...
-    signParts{1}, [signParts{2}, '.csv']);
-  if exist(cachePath, 'file')
-    descriptors = single(dlmread(cachePath, ';')');
-    if numel(signParts) == 3
-      descriptors = descriptors(:, str2double(signParts{3}));
-    end
-    return;
+signParts = strsplit(signature, '.');
+cachePath = fullfile(opts.cachePath, opts.cacheName, ...
+  signParts{1}, [signParts{2}, '.csv']);
+if ~isempty(opts.cacheName) && exist(cachePath, 'file')
+  descriptors = single(dlmread(cachePath, ';')');
+else
+  patches = imdb.getPatches([signParts{1}, '.' signParts{2}]);
+  patches = reshape(patches, size(patches, 1), size(patches, 2), []);
+  descriptors = single(descfun(signature, patches));
+  assert(size(descriptors, 2) == size(patches, 3), ...
+    'Invalid number of descriptors returned.');
+  if ~isempty(opts.cacheName)
+    vl_xmkdir(fileparts(cachePath));
+    dlmwrite(cachePath, descriptors', ';');
   end
 end
-
-patches = imdb.getPatches(signature);
-patches = reshape(patches, size(patches, 1), size(patches, 2), []);
-
-stime = tic;
-descriptors = single(descfun(signature, patches));
-proctime = toc(stime);
-assert(size(descriptors, 2) == size(patches, 3), ...
-  'Invalid number of descriptors returned.');
-
-if ~isempty(opts.cacheName) && numel(signParts) == 2
-  vl_xmkdir(fileparts(cachePath));
-  dlmwrite(cachePath, descriptors', ';');
+if numel(signParts) == 3
+  descriptors = descriptors(:, str2double(signParts{3}));
 end
 end
 
