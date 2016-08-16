e4f7a250cf6e971c554fd239d339794fc72101f0
Karel Lenc <lenckarel@gmail.com>
2016-08-16 18:08:19 +0100

    bugfix.

--- matlab/desc_patch_resize.m
+++ matlab/desc_patch_resize.m
@@ -3,7 +3,7 @@ function desc = desc_patch_resize(numbins, signature, patches)
 patches = single(squeeze(patches));
 meanVal = reshape(mean(mean(patches, 1), 2), 1, 1, []);
 patchSz = [size(patches, 1), size(patches, 2)];
-stdVal = std(reshape(single(patches), prod(patchSz(:)), []), 0, 1);
+stdVal = reshape(std(reshape(single(patches), prod(patchSz(:)), []), 0, 1), 1, 1, []);
 patches = bsxfun(@rdivide, bsxfun(@minus, patches, meanVal), stdVal);
 
 desc = zeros(numbins^2, size(patches, 3), 'single');
