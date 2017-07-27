742928087c42f7789260dcd130ce2be99701c55e
Karel Lenc <lenckarel@gmail.com>
2017-07-27 10:35:26 +0100

    Simplified the provision script. Uses just MATLAB, not wget.

--- matlab/+utls/provision.m
+++ matlab/+utls/provision.m
@@ -6,24 +6,11 @@ if ~exist(url_file, 'file')
 end;
 [~, url_file_nm] = fileparts(url_file);
 done_file = fullfile(tgt_dir, ['.', url_file_nm, '.done']);
-if exist(tgt_dir, 'dir')
-  if ~exist(done_file, 'file')
-    if ~override
-      res = input('Target directory %s already exist. Continue? [Y/N] Y ', ...
-        's');
-    end
-    if override || ismember(lower(res), {'n', 'no'})
-      create_done(done_file);
-      return;
-    end
-  end
-else
-  mkdir(tgt_dir);
-end
-if exist(done_file, 'file'), return; end;
+if ~exist(tgt_dir, 'dir'), mkdir(tgt_dir); end
+if exist(done_file, 'file') && ~override, return; end;
 url = utls.readfile(url_file);
 for ui = 1:numel(url)
-  p_untar(url{ui}, tgt_dir);
+  unpack(url{ui}, tgt_dir);
 end
 downloaded = true;
 create_done(done_file);
@@ -34,34 +21,17 @@ f = fopen(done_file, 'w'); fclose(f);
 fprintf('To reprovision, delete %s.\n', done_file);
 end
 
-function p_untar(url, tgt_dir)
-[~, wget_p] = system('which wget');
-if exist(strtrim(wget_p), 'file');
-  [~, fname, ext] = fileparts(url);
-  tar_file = fullfile(tgt_dir, [fname, ext]);
-  if ~exist(tar_file, 'file')
-    fprintf(isdeployed+1, 'Downloading %s -> %s.\n', url, tar_file);
-    ret = system(sprintf('wget %s -O %s', url, tar_file));
-    if ret ~= 0
-      fprintf(isdeployed+1, 'wget failed.');
-      delete(tar_file);
-    end;
-  end
-  if exist(tar_file, 'file')
-    fprintf(isdeployed+1, 'Unpacking %s -> %s. This may take a while...\n', ...
-      tar_file, tgt_dir);
-    untar(tar_file, tgt_dir);
-  else
-    m_untar(url, tgt_dir);
-  end
-else
-  m_untar(url, tgt_dir);
-end
-end
-
-function m_untar(url, tgt_dir)
+function unpack(url, tgt_dir)
+[~,~,ext] = fileparts(url);
 fprintf(isdeployed+1, ...
-  'Downloading %s -> %s using MATLAB, this may take a while...\n',...
+  'Downloading %s -> %s, this may take a while...\n',...
   url, tgt_dir);
-untar(url, tgt_dir);
+switch ext
+  case '.tar'
+    untar(url, tgt_dir);
+  case '.zip'
+    unzip(url, tgt_dir);
+  otherwise
+    error('Unknown archive %s.', ext);
+end
 end
--- matlab/lib/vlfeat/vlfeat_setup.m
+++ matlab/lib/vlfeat/vlfeat_setup.m
@@ -3,7 +3,7 @@ function vlfeat_setup( )
 assert(exist('hb_path', 'file') == 2, 'HB not found.');
 
 vlf_dir = fullfile(hb_path, 'matlab', 'lib', 'vlfeat');
-utls.provision(fullfile(vlf_dir, 'vlfeat.url'), vlf_dir, true);
+utls.provision(fullfile(vlf_dir, 'vlfeat.url'), vlf_dir);
 vlf_dist_dir = dir(fullfile(vlf_dir, 'vlfeat*'));
 vlf_dist_dir = vlf_dist_dir([vlf_dist_dir.isdir]);
 run(fullfile(vlf_dir, vlf_dist_dir(1).name, 'toolbox', 'vl_setup.m'));
