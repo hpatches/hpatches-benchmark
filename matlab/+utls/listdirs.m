ec3eb1ea4470d988f76d82904d8f3186c75a16c4
Karel Lenc <lenckarel@gmail.com>
2017-07-27 14:38:19 +0100

    Bugfix - throw a sensible error in case of non existent directory.

--- matlab/+utls/listdirs.m
+++ matlab/+utls/listdirs.m
@@ -1,4 +1,5 @@
 function dirs = listdirs(path)
+assert(isdir(path), sprintf('%s not a directory.', path));
 dirs = dir(path);
 is_valid = [dirs.isdir] & arrayfun(@(d) d.name(1)~='.', dirs)';
 dirs = {dirs.name};
