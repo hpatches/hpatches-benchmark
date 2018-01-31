d18784912914d81795cc68b904065b5dcebb73eb
Karel Lenc <lenckarel@gmail.com>
2018-01-31 15:12:08 +0000

    Added a separate script for the trained descriptors.

--- matlab/run_article_cval.m
+++ matlab/run_article_cval.m
@@ -6,11 +6,7 @@ descs = {'resize', 'sift', 'binboost'  'brief'  'deepdesc', ...
   'rootsift'  'siam'  'siam2stream',  'tfeat-margin'  ...
   'tfeat-margin-star'  'tfeat-ratio'  'tfeat-ratio-star', ...
   'kde', 'mkd', 'wlrn', 'tfeat-n-lib', 'l2net', 'tnet', ...
-  'HardNetLib', 'HardNetLib+', ...
-  'tfeat-n-train-a', 'tfeat-n-train-b', 'tfeat-n-train-c', 'tfeat-n-train-view', 'tfeat-n-train-illum', ...
-  'hpa', 'hpb', 'hpc', 'hp_il', 'hp_view'};
-nonorm = {'hpa', 'hpb', 'hpc', 'hp_il', 'hp_view'};
-%descs = {'meanstd'};
+  'HardNetLib', 'HardNetLib+'};
 global_args = {'num_neg', inf, 'numtype', 'double', ...
   'scoresroot', fullfile(hb_path, 'matlab', 'scores', 'scores_all_cval')};
 splits = {'a', 'b', 'c'};
@@ -22,25 +18,10 @@ norms.Properties.RowNames = norms.descriptor;
 args = {};
 for di = 1:numel(descs)
   % Add original descriptor
-  if contains(descs{di}, '-train-')
-    % Those are already defined three times
-    args{end+1} = [descs(di), global_args, {'split', descs{di}(end)}];
-  else
-    args{end+1} = [descs(di), global_args, {'split', splits}];
-  end
-  if ismember(descs{di}, nonorm)
-    fprintf('Skipping norm for %s\n', descs{di});
-    continue;
-  end
+  args{end+1} = [descs(di), global_args, {'split', splits}];
   % Add best norm descriptor, diff norm per split
   for spi = 1:numel(splits)
-    if contains(descs{di}, '-train-')
-      desc_name = descs{di};
-      det_split = descs{di}(end);
-      if ~strcmp(det_split, splits{spi}), continue; end
-    else
-      desc_name = [descs{di}, '-train-', splits{spi}];
-    end
+    desc_name = [descs{di}, '-train-', splits{spi}];
     args{end+1} = [{descs{di}, 'norm', true, 'norm_split', splits{spi}, ...
       'normstring', norms{desc_name, 'normstr'}{1}}, ...
       'split', splits{spi}, global_args];
