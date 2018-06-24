d37e6a8138b154dc908dcf53aeaf58b110b31115
Karel Lenc <lenckarel@gmail.com>
2018-06-24 13:46:54 +0100

    Added additional pami experiments.

---
+++ matlab/run_article_pami_trained.m
@@ -0,0 +1,30 @@
+% Recompute all scores for all descriptors.
+% Requires approximately at least 64GB RAM, unless `num_neg` is decreased.
+
+descs = {};
+
+descs{end+1} = struct('name', 'hp-train-a', 'split', {{'full'}}); 
+descs{end+1} = struct('name', 'hp-train-illum', 'split', {{'full'}}); 
+descs{end+1} = struct('name', 'hp-train-view', 'split', {{'full'}}); 
+
+descs{end+1} = struct('name', 'tfeat-n-train-a', 'split', {{'full'}}); 
+descs{end+1} = struct('name', 'tfeat-n-train-illum', 'split', {{'full'}}); 
+descs{end+1} = struct('name', 'tfeat-n-train-view', 'split', {{'full'}}); 
+descs = cell2mat(descs);
+
+global_args = {'num_neg', inf, 'numtype', 'double', ...
+  'scoresroot', fullfile(hb_path, 'matlab', 'scores', 'scores_pami_trained')};
+
+%%
+args = {};
+for di = 1:numel(descs)
+    args{end+1} = [{descs(di).name, 'norm', false, ...
+      'split', descs(di).split}, global_args];
+end
+fprintf('%d tasks.\n', numel(args));
+
+%%
+sel = utls.parallelise(1:numel(args));
+parfor ai = 1:size(sel, 1)
+  hb('all', args{sel(ai)}{:});
+end
